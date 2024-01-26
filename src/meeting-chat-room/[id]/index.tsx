eee43202f5877acf2f28feef991dfbd3349d6570
kimgorok <kjyse2@naver.com>
2024-01-26 21:48:40 +0900

    🛠채팅방 코드 줄이기(불안)

--- src/meeting-chat-room/[id]/index.tsx
+++ src/meeting-chat-room/[id]/index.tsx
@@ -31,7 +31,12 @@ import ChatWindow from "../components/ChatWindow";
       그리고 Publish는 그냥 메시지를 서버(/pub/send)로 보내는 함수 
       Disconnect는 제곧내 */
 
-// MeetingChatRoom 함수
+interface IChat {
+  createdAt: string;
+  nickname: string;
+  content: string;
+}
+
 export default function MeetingChatRoom() {
   const navigate = useNavigate();
   const { id } = useParams();
@@ -45,7 +50,6 @@ export default function MeetingChatRoom() {
     const data = await response.json();
     return data;
   };
-
   const { data: profile } = useQuery(["profile"], getUserProfile);
 
   // 기존의 채팅 데이터 가져오기
@@ -79,8 +83,6 @@ export default function MeetingChatRoom() {
 
   const getGroup = async () => {
     try {
-      // getApi 함수를 사용하여 외부 API에서 데이터를 가져옴
-      // API 엔드포인트 경로는 `/groups/${id}`로 지정되며, id는 외부에서 전달되는 매개변수
       const response = await getApi({ link: `/groups/${id}` });
       const data = await response.json();
       return data;
@@ -89,32 +91,21 @@ export default function MeetingChatRoom() {
       throw error; // 에러를 상위로 전파
     }
   };
-
-  // useQuery 훅을 사용하여 데이터를 가져오는 부분
   const { data: group } = useQuery(["group", id], getGroup, {
     enabled: !!id, // enabled 옵션을 사용하여 id가 존재할 때에만 데이터를 가져오도록 설정
   });
 
   // 제네릭으로 타입 명시
-  const [chatList, setChatList] = useState<
-    {
-      createdAt: string;
-      nickname: string;
-      content: string;
-    }[]
-  >([]); // 채팅 메시지 리스트
+  const [chatList, setChatList] = useState<IChat[]>([]); // 채팅 메시지 리스트
   const [message, setMessage] = useState(""); // 사용자가 보낼 메시지
-  const [sender, setSender] = useState(decoded.sub); // 발신자
-  const [groupId, setGroupId] = useState(id); // 그룹 ID
-  const [topicUrl, setTopicUrl] = useState(id); // 토픽 URL
+
   // 미티 웹소켓 주소
   const brokerUrl =
     "ws://mitiappserver-env.eba-r2uts3k4.ap-northeast-2.elasticbeanstalk.com/ws/chat/websocket";
+  console.log(brokerUrl);
   // client 객체가 StompJs.Client 타입을 따르도록
   const client = useRef<StompJs.Client>(null!); // WebSocket 클라이언트 객체
   // useRef 초기값을 null이 아님으로 설정
-
-  //
 
   // Connect
   // WebSocket 연결 함수
@@ -135,7 +126,6 @@ export default function MeetingChatRoom() {
   const publish = (chat: string) => {
     // 웹소켓 연결이 없으면 함수를 종료 (보호 조건)
     if (!client.current.connected) return;
-
     // StompJs의 publish 메서드를 사용하여 채팅 메시지를 발행
     client.current.publish({
       destination: "/pub/send", // 메시지를 보낼 때에는 여기로
@@ -147,14 +137,12 @@ export default function MeetingChatRoom() {
   // 특정 토픽 구독 함수
   const subscribe = () => {
     // 토픽 URL이 없으면 함수 종료. 토픽 없으면 구독 안함
-    if (!topicUrl) {
-      console.log("끊김");
+    if (!id) {
       return;
     }
-
     // StompJs의 subscribe 메서드를 사용하여 특정 토픽을 구독
     // subscribe는 서버에서 해당 토픽으로 메시지가 전달될 때마다 콜백 함수가 실행
-    client.current.subscribe(`/sub/${topicUrl}`, (body: { body: string }) => {
+    client.current.subscribe(`/sub/${id}`, (body: { body: string }) => {
       /* 서버에서 받은 body 객체에서 body 속성을 추출하고 
         이를 JSON 형태로 파싱하여 채팅 메시지로 사용 */
       const json_body = JSON.parse(body.body);
@@ -173,72 +161,43 @@ export default function MeetingChatRoom() {
     client.current.deactivate();
   };
 
-  //
-
   // 입력값이 변경될 때마다 상태를 업데이트 하는 이벤트 핸들러 함수들
   // 채팅 메시지
   const handleChangeMessage = (event: React.ChangeEvent<HTMLInputElement>) => {
     setMessage(event.target.value);
   };
-  // 그룹 ID
-  const handleChangeGroupId = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // parseInt 또는 Number 함수를 사용하여 문자열을 숫자로 변환
-    setGroupId(event.target.value);
-  };
-  // 발신자
-  const handleChangeSender = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSender(event.target.value);
-  };
-  // 토픽
-  const handleTopicChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTopicUrl(event.target.value);
-  };
-
-  //
 
   // 폼 제출 이벤트 핸들러 함수
   const handleSubmit = (
     event: { preventDefault: () => void }, // 제출 이벤트
     message: string,
-    sender: string | undefined,
-    groupId: number | string | unknown
+    id: number | string | unknown
   ) => {
     event.preventDefault();
-
     // 메시지가 공백인 경우 전송하지 않음
     if (message.trim() === "") {
       return;
     }
-
     // 발행 함수 호출을 통해 메시지 전송, stomp 프로토콜로 websocket을 통해 메시지 발송
     publish(
       JSON.stringify({
         // 정보를 JSON 문자열로 변환
         message: message,
-        sender: sender,
-        groupId: groupId,
+        sender: decoded.sub,
+        groupId: id,
       })
     );
-
     setMessage("");
   }; // 제출된 JSON문자열은 서버로 전송됨
-
-  //
 
   // 컴포넌트가 마운트될 때 한 번만 호출되는 효과 훅
   useEffect(() => {
     connect(); // // 컴포넌트가 마운트될 때 WebSocket 연결
-
-    // 컴포넌트가 언마운트될 때 WebSocket 연결 해제
-    return () => disconnect();
+    return () => disconnect(); // 컴포넌트가 언마운트될 때 WebSocket 연결 해제
   }, []);
 
   // 메뉴 표시
   const [showRightMenu, setShowRightMenu] = useState(false);
-
-  const onClickMenu = () => {
-    setShowRightMenu(true);
-  };
 
   const setRoomDelted = useSetRecoilState(SnackBarAtom);
   const handleDeleteRoom = () => {
@@ -265,7 +224,13 @@ export default function MeetingChatRoom() {
       <TopBar
         title={`${group?.title}`}
         leftIcon={<ArrowbackIcon onClick={() => navigate(-1)} />}
-        rightIcon={<HamburgerIcon onClick={onClickMenu} />}
+        rightIcon={
+          <HamburgerIcon
+            onClick={() => {
+              setShowRightMenu(true);
+            }}
+          />
+        }
       />
       <MeetingChatRoomScreen>
         <ChatWindowContainer>
@@ -276,34 +241,13 @@ export default function MeetingChatRoom() {
           <label htmlFor="topic-url" hidden>
             Topic URL:
           </label>
-          <input
-            type="number"
-            id="topic-url"
-            value={topicUrl}
-            onChange={handleTopicChange}
-            hidden
-          />
+          <input type="number" id="topic-url" value={id} hidden />
           <button hidden onClick={() => subscribe()}>
             Subscribe
           </button>
         </div>
-        <form
-          onSubmit={(event) => handleSubmit(event, message, sender, groupId)}
-        >
-          <input
-            placeholder="sender"
-            type="text"
-            value={sender}
-            onChange={handleChangeSender}
-            hidden
-          />
-          <input
-            placeholder="groupId"
-            type="number"
-            value={id}
-            onChange={handleChangeGroupId}
-            hidden
-          />
+        <form onSubmit={(event) => handleSubmit(event, message, id)}>
+          <input placeholder="groupId" type="number" value={id} hidden />
           <ChattingInputDiv>
             <ChattingInput
               placeholder="메시지 입력"
