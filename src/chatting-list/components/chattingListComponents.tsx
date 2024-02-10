bd7332b8f709a983f50f713c51c1114e6eb41204
kimgorok <kjyse2@naver.com>
2024-02-10 21:35:27 +0900

    🌱🎨채팅리스트 세팅

---
+++ src/chatting-list/components/chattingListComponents.tsx
@@ -0,0 +1,96 @@
+import styled from "styled-components";
+import { Screen } from "../../components/Screen";
+
+export interface LastMessages {
+  [groupId: string]: {
+    content: string;
+    createdAt: string;
+  };
+}
+export const ChattingListScreen = styled(Screen)`
+  padding: 0;
+  padding-top: 56px;
+  padding-bottom: 64px;
+`;
+
+export const ChattingWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+`;
+
+export const ChattingFrame = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  padding: 16px 24px;
+  gap: 16px;
+  border-bottom: 1px solid #f2f0ef;
+`;
+
+export const TitleMemberTimeFrame = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TitleMemberFrame = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+export const TitleText = styled.span`
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 16px;
+  letter-spacing: -0.016em;
+`;
+
+export const MemberText = styled.span`
+  font-size: 14px;
+  line-height: 16px;
+  letter-spacing: -0.016em;
+  color: #a7a7a7;
+`;
+
+export const TimeText = styled.span`
+  font-size: 12px;
+  line-height: 12px;
+  letter-spacing: -0.01em;
+  color: #a7a7a7;
+`;
+
+export const ChatAlertFrame = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+export const ChatText = styled.span`
+  font-size: 13px;
+  line-height: 14px;
+  letter-spacing: -0.01em;
+  color: #a7a7a7;
+`;
+
+export const AlertCircle = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 14px;
+  height: 14px;
+  background: #ff7152;
+  border-radius: 100px;
+`;
+
+export const AlertCount = styled.span`
+  font-size: 10px;
+  line-height: 12px;
+  letter-spacing: -0.01em;
+  color: #ffffff;
+`;
--- src/chatting-list/index.tsx
+++ src/chatting-list/index.tsx
@@ -1,3 +1,99 @@
+import { TopBar } from "../components/TopBar";
+import { TabBar } from "../components/TabBar";
+import { useNavigate } from "react-router-dom";
+import { getApi } from "../api/getApi";
+import { useQuery } from "react-query";
+import { Key, useEffect, useState } from "react";
+import {
+  TitleMemberTimeFrame,
+  ChattingFrame,
+  ChattingListScreen,
+  ChattingWrapper,
+  LastMessages,
+  TitleMemberFrame,
+  TitleText,
+  MemberText,
+  TimeText,
+  ChatAlertFrame,
+  ChatText,
+  AlertCircle,
+  AlertCount,
+} from "./components/chattingListComponents";
+
 export default function ChattingList() {
-  return <h1>하이</h1>;
+  const navigate = useNavigate();
+
+  const getMyGroups = () =>
+    getApi({ link: "/groups/my" }).then((response) => response.json());
+  const { data } = useQuery(["myGroups"], getMyGroups);
+
+  const getChat = (groupId: string) =>
+    getApi({ link: `/message/${groupId}` }).then((response) => response.json());
+
+  const [lastMessages, setLastMessages] = useState<LastMessages>({});
+
+  useEffect(() => {
+    if (data?.content) {
+      data.content.forEach((group: { id: string }) => {
+        getChat(group.id).then((chatData) => {
+          const lastMessage = chatData[chatData.length - 1];
+          setLastMessages((prevMessages) => ({
+            ...prevMessages,
+            [group.id]: lastMessage,
+          }));
+        });
+      });
+    }
+  }, [data]);
+
+  const formatTime = (timeString: string) => {
+    const date = new Date(timeString);
+    if (isNaN(date.getTime())) {
+      return ""; // 유효하지 않은 날짜인 경우 빈 문자열 반환
+    }
+    return date.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    });
+  };
+  return (
+    <>
+      <TopBar title="채팅" />
+      <ChattingListScreen>
+        <ChattingWrapper>
+          {data?.content.map(
+            (
+              group: {
+                title: string;
+                nowUsers: string;
+                id: string;
+              },
+              index: Key
+            ) => (
+              <ChattingFrame key={index}>
+                <TitleMemberTimeFrame>
+                  <TitleMemberFrame>
+                    <TitleText>{group.title}</TitleText>
+                    <MemberText>{group.nowUsers}</MemberText>
+                  </TitleMemberFrame>
+                  <TimeText>
+                    {formatTime(lastMessages[group.id]?.createdAt)}
+                  </TimeText>
+                </TitleMemberTimeFrame>
+
+                <ChatAlertFrame>
+                  <ChatText>{lastMessages[group.id]?.content}</ChatText>
+                  <AlertCircle>
+                    <AlertCount>10</AlertCount>
+                  </AlertCircle>
+                </ChatAlertFrame>
+              </ChattingFrame>
+            )
+          )}
+        </ChattingWrapper>
+      </ChattingListScreen>
+      <TabBar />
+    </>
+  );
 }
