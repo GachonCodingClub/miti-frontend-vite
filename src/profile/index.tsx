4a1397befd006447241f4bc7c4b7525980bf3114
kimgorok <kjyse2@naver.com>
2024-02-10 17:34:04 +0900

    ✅프로필_이제 내 프로필 보임

--- src/profile/index.tsx
+++ src/profile/index.tsx
@@ -54,13 +54,26 @@ export default function Profile() {
     setNowDate(date.toString());
   }, [isMine]);
 
-  const navigate = useNavigate(); // useNavigate 사용
+  const navigate = useNavigate();
 
   const getMyGroups = () =>
     getApi({ link: "/groups/my" }).then(
       (response) => response.json() as Promise<IGroups>
     );
   const { data, isLoading } = useQuery(["myGroups"], getMyGroups);
+
+  const getUserProfile = async () => {
+    const response = await getApi({ link: `/users/profile/my` });
+    const data = await response.json();
+    return data;
+  };
+  const { data: profile } = useQuery(["profile"], getUserProfile);
+
+  useEffect(() => {
+    console.log(data);
+    console.log(profile);
+  }, []);
+
   const currentGroups = data?.content?.filter((group) => {
     const date = group.meetDate;
     return date > nowDate;
@@ -81,18 +94,21 @@ export default function Profile() {
         <ProfileScreen>
           <ProfileBox>
             <ProfileInfo>
-              <span className="font-medium text-base text-gray-800">쿵야</span>
+              <span className="font-medium text-base text-gray-800">
+                {profile?.nickname}
+              </span>
               <div className="flex gap-2 font-normal text-sm text-gray-500">
-                <span>24살</span>
-                <span>남자</span>
-                <span>170cm</span>
-                <span>80kg</span>
+                <span>{profile?.age}살</span>
+                <span>{profile.gender === "MALE" ? "남자" : "여자"}</span>
+                <span>{profile.height}cm</span>
+                <span>{profile.weight}kg</span>
               </div>
             </ProfileInfo>
             <span className="font-normal text-xs text-gray-800">
-              discription...
+              {profile.description}
             </span>
           </ProfileBox>
+          {/* 미팅리스트 */}
           <ProfileMeetings>
             <MeetingTabBar>
               <div
