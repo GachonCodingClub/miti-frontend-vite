711aa7d59127aef1c566e3ceea2e0c3ca2073705
kimgorok <kjyse2@naver.com>
2024-02-17 18:25:31 +0900

    🔮🛠getHeaders 적용

--- src/components/getHeaders.ts
+++ src/components/getHeaders.ts
@@ -1,4 +1,4 @@
-export function getHeaders(token: string) {
+export function getHeaders(token: string | null) {
   return {
     "Content-Type": "application/json",
     Authorization: `Bearer ${token}`,
