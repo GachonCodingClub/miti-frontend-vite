de4fc135f73531ef58ca7db375323c6eeb6b6015
kimgorok <kjyse2@naver.com>
2024-02-21 23:11:59 +0900

    🐢미팅 참가 신청_그룹으로 신청

--- src/meeting-list/[id]/index.tsx
+++ src/meeting-list/[id]/index.tsx
@@ -28,12 +28,19 @@ import {
   MemberInfo,
   MemberDetail,
   JoinButtonBox,
+  AddMemberWrapper,
 } from "../components/meetingDetail.Components";
 import { useNavigate, useParams } from "react-router-dom";
 import { Overlay } from "../../sign-up/styles/detailComponents";
 import { JwtPayload, jwtDecode } from "jwt-decode";
 import { formatDate } from "../../utils";
 import { getHeaders } from "../../components/getHeaders";
+import {
+  AddMemberButton,
+  AddMemberText,
+} from "../../create-meeting/styles/createMeetingDetailComponents";
+import { MyInputBoxSVG } from "../../components/MyInputBox";
+import AdditionalParticipantsList from "../../create-meeting/components/additionalParticipantsList";
 
 export default function MeetingDetail() {
   useLoginGuard();
@@ -86,8 +93,78 @@ export default function MeetingDetail() {
     console.log("그룹", group);
   }, []);
 
+  // 닉네임으로 참여자 추가(선택 입력)
+  const [additionalParticipants, setAdditionalParticipants] = useState<
+    string[]
+  >([]);
+  const [inputAdditionalNickname, setInputAdditionalNickname] = useState("");
+  const [nonExistentDialog, setNonExistentDialog] = useState(false);
+  const [addMyNicknameDialog, setAddMyNicknameDialog] = useState(false);
+  const [duplicateOrBlankErrorDialog, setDuplicateOrBlankErrorDialog] =
+    useState(false);
+  const [showAdd, setShowAdd] = useState(false);
+  const [cannotAddDialog, setCannotAddDialog] = useState(false);
+
+  const nowMember = (parties?.acceptedParties[0]?.users.length ?? 0) + 1;
+  const AddMember = additionalParticipants.length + 1;
+  const finalMember = nowMember + AddMember;
+
+  const onRemoveNicknameClick = (index: number) => {
+    // additionalParticipants배열을 복사하여 updatedParticipants에 저장
+    // 직접 상태 수정하지 않고, 새 배열을 생성해 불변성을 유지하기 위해
+    const updatedParticipants = [...additionalParticipants];
+    /* splice method를 사용하여 updatedParticipants 배열에서 index에 해당하는 
+      위치의 닉네임을 1개 제거 */
+    updatedParticipants.splice(index, 1);
+    setAdditionalParticipants(updatedParticipants);
+  };
+
+  const onAddNicknameClick = async () => {
+    try {
+      const response = await getApi({
+        link: `/auth/check/nickname?nickname=${inputAdditionalNickname}`,
+      }); // 존재하는 닉네임인지 체크, 결과를 response에 저장
+
+      // trim()으로 문자열 뒤 공백 제거
+      const trimmedNickname = inputAdditionalNickname.trim();
+
+      // 추가된 닉네임 배열에 빈 문자열이 있는지 확인
+      if (trimmedNickname === "") {
+        return;
+      }
+
+      // response의 상태 코드가 409(중복)라면
+      if (response.status !== 409) {
+        setNonExistentDialog(true); // 존재하지 않는 닉네임 추가하려 하면 오류 띄우기
+      } else {
+        // 이미 추가한 닉네임 중 중복된 닉네임이 없으면 추가
+        if (additionalParticipants.includes(trimmedNickname)) {
+          setDuplicateOrBlankErrorDialog(true); // 이미 추가한 닉네임 추가하려 하면 오류 띄우기
+          return;
+        }
+
+        if (finalMember >= (group?.maxUsers ?? 0)) {
+          setCannotAddDialog(true);
+          return;
+        }
+
+        // 닉네임이 myNickname과 같은지 확인
+        if (trimmedNickname === profile?.nickname) {
+          setAddMyNicknameDialog(true); // 본인 닉네임 추가 못하게 막기
+          return;
+        }
+
+        // 그렇지 않다면 추가된 닉네임 배열 끝에 inputAdditionalNickname 추가
+        setAdditionalParticipants([...additionalParticipants, trimmedNickname]);
+        setInputAdditionalNickname(""); // 추가 후 입력 필드를 지움
+      }
+    } catch (error) {
+      console.error("비동기 작업 중 오류 발생!!!!", error);
+    }
+  };
+
   const bodyData = {
-    nicknames: [],
+    nicknames: [...additionalParticipants],
   };
   const headers = getHeaders(token);
 
@@ -215,14 +292,53 @@ export default function MeetingDetail() {
               </MemberInfo>
             </DetailMember>
           </DetailContents>
+
           <JoinButtonBox>
             {decodedToken?.sub == group?.leaderUserSummaryDto?.userId ||
             group?.groupStatus == "CLOSE" ? (
               <LongWhiteBtn text="신청할 수 없어요" onClick={() => {}} />
             ) : (
-              <LongOrangeBtn text="미팅 참여 신청" onClick={onSubmitClick} />
+              <LongOrangeBtn
+                text="미팅 참여 신청"
+                onClick={() => {
+                  setShowAdd(true);
+                }}
+              />
             )}
           </JoinButtonBox>
+          {showAdd && (
+            <>
+              <AddMemberWrapper>
+                <AddMemberText>
+                  닉네임으로 본인 외의 참여자 추가(선택 입력)
+                </AddMemberText>
+                <div className="flex items-center">
+                  <MyInputBoxSVG
+                    onClick={() => {}}
+                    label=""
+                    value={inputAdditionalNickname}
+                    onChange={(e) => setInputAdditionalNickname(e.target.value)}
+                    placeholder="추가 참여자 닉네임(선택 입력)"
+                    type="text"
+                  />
+                  <AddMemberButton onClick={onAddNicknameClick}>
+                    +
+                  </AddMemberButton>
+                </div>
+              </AddMemberWrapper>
+              <JoinButtonBox>
+                <LongOrangeBtn text="신청하기" onClick={onSubmitClick} />
+              </JoinButtonBox>
+            </>
+          )}
+          {/* 추가된 닉네임들을 표시 */}
+
+          {showAdd && additionalParticipants.length > 0 ? (
+            <AdditionalParticipantsList
+              participants={additionalParticipants}
+              onRemoveNicknameClick={onRemoveNicknameClick}
+            />
+          ) : null}
         </DetailBox>
 
         {showDialog && (
@@ -233,6 +349,7 @@ export default function MeetingDetail() {
               right="닫기"
               onRightClick={() => {
                 setShowDialog(false);
+                setShowAdd(false);
               }}
             />
           </Overlay>
@@ -246,6 +363,58 @@ export default function MeetingDetail() {
               onRightClick={() => {
                 setShowErrorDialog(false);
               }}
+            />
+          </Overlay>
+        )}
+
+        {cannotAddDialog && (
+          <Overlay>
+            <DialogOneBtn
+              title="미팅 정원보다 많이 추가할 수 없어요."
+              contents=""
+              onRightClick={() => {
+                setCannotAddDialog(false);
+              }}
+              right="닫기"
+            />
+          </Overlay>
+        )}
+
+        {duplicateOrBlankErrorDialog && (
+          <Overlay>
+            <DialogOneBtn
+              title="닉네임을 확인해 주세요."
+              contents="중복된 닉네임이나 빈칸이 있어요."
+              onRightClick={() => {
+                setDuplicateOrBlankErrorDialog(false);
+              }}
+              right="닫기"
+            />
+          </Overlay>
+        )}
+
+        {addMyNicknameDialog && (
+          <Overlay>
+            <DialogOneBtn
+              title="본인은 추가할 수 없어요."
+              contents=""
+              onRightClick={() => {
+                setAddMyNicknameDialog(false);
+              }}
+              right="닫기"
+            />
+          </Overlay>
+        )}
+
+        {nonExistentDialog && (
+          <Overlay>
+            <DialogOneBtn
+              title="존재하지 않는 닉네임이에요."
+              contents=""
+              onRightClick={() => {
+                setNonExistentDialog(false);
+              }}
+              right="닫기"
             />
           </Overlay>
         )}
