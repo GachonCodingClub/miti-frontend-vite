5196717d7eb40e4c9ab629df2245e730e3353a33
kimgorok <kjyse2@naver.com>
2024-02-25 22:12:44 +0900

    [DEPLOY] 🔮 .env수정

--- src/meeting-chat-room/[id]/index.tsx
+++ src/meeting-chat-room/[id]/index.tsx
@@ -71,6 +71,7 @@ export default function MeetingChatRoom() {
 
   // 미티 웹소켓 주소
   const brokerUrl = `${import.meta.env.VITE_WEBSOCKET_URL}`;
+  console.log(brokerUrl);
   // client 객체가 StompJs.Client 타입을 따르도록
   const client = useRef<StompJs.Client>(null!); // WebSocket 클라이언트 객체
   // useRef 초기값을 null이 아님으로 설정
