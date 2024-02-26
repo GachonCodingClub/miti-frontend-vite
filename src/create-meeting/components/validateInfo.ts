01711337f1b1fe6df5d065b51b149d7efdea363c
kimgorok <kjyse2@naver.com>
2024-02-26 21:37:04 +0900

    [DEPLOY] 🔮미팅만들기/수정하기_오늘 이전의 날짜 선택 시 POST 불가

--- src/create-meeting/components/validateInfo.ts
+++ src/create-meeting/components/validateInfo.ts
@@ -19,7 +19,15 @@ export const validateForm = ({
   setMemberError,
   setDuplicateBlankErrorDialog,
 }: IValidationProps): boolean => {
-  setDateError(selecteDate === "" ? "날짜를 선택해 주세요." : "");
+  const currentDate = new Date().toISOString().split("T")[0]; // 현재 날짜
+  if (selecteDate === "") {
+    setDateError("날짜를 선택해 주세요.");
+  } else if (selecteDate < currentDate) {
+    setDateError("날짜는 오늘 이후여야 합니다.");
+  } else {
+    setDateError(""); // 오류가 없을 경우 오류 메시지를 비웁니다.
+  }
+
   setPlaceError(inputPlace === "" ? "장소를 입력해 주세요." : "");
   setMemberError(numericInputMember < 2 ? "인원은 2 이상이어야 합니다." : "");
 
@@ -30,6 +38,7 @@ export const validateForm = ({
 
   return (
     selecteDate !== "" &&
+    selecteDate >= currentDate &&
     inputPlace !== "" &&
     numericInputMember >= 2 &&
     !additionalParticipants.includes("")
