b22ac0a4bd5d0f5a46eaeedec830bd348dc9f573
kimgorok <kjyse2@naver.com>
2024-02-28 05:03:57 +0900

    [DEPLOY] 푸시 알림 테스트

--- src/profile/setting/index.tsx
+++ src/profile/setting/index.tsx
@@ -8,9 +8,30 @@ import {
   LogoutFrame,
 } from "../styles/settingComponents";
 import { PaddingScreen } from "../../components/styles/Screen";
+import { PushNotifications } from "@capacitor/push-notifications";
 
 export default function Setting() {
   const navigate = useNavigate();
+
+  // 푸시 알림 등록 함수
+  const registerNotifications = async () => {
+    let permStatus = await PushNotifications.checkPermissions();
+    if (permStatus.receive === "prompt") {
+      permStatus = await PushNotifications.requestPermissions();
+    }
+    if (permStatus.receive === "granted") {
+      await PushNotifications.register();
+      alert("푸시 알림이 활성화되었습니다.");
+    } else {
+      alert("푸시 알림 권한이 거부되었습니다.");
+    }
+  };
+
+  // 푸시 알림 해제 함수
+  const unregisterNotifications = async () => {
+    await PushNotifications.removeAllListeners();
+    alert("푸시 알림이 비활성화되었습니다.");
+  };
   return (
     <>
       <TopBar
@@ -24,6 +45,20 @@ export default function Setting() {
           <SettingButton to="/report">신고하기</SettingButton>
           <SettingButton to="/agreement">서비스 이용약관</SettingButton>
           <SettingButton to="/setting/withdrawal">회원 탈퇴하기</SettingButton>
+
+          <button
+            onClick={registerNotifications}
+            style={{ padding: 10, marginTop: 20, cursor: "pointer" }}
+          >
+            푸시 알림 활성화
+          </button>
+          {/* 푸시 알림 해제 버튼 */}
+          <button
+            onClick={unregisterNotifications}
+            style={{ padding: 10, marginTop: 20, cursor: "pointer" }}
+          >
+            푸시 알림 비활성화
+          </button>
         </SettingButtonBox>
       </PaddingScreen>
       <LogoutFrame>
