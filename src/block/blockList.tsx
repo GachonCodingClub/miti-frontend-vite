5ff4d457cc872f50c62ad38006f26d801562fc7f
kimgorok <kjyse2@naver.com>
2024-03-07 01:51:39 +0900

    🐢차단 목록 진행

--- src/block/blockList.tsx
+++ src/block/blockList.tsx
@@ -3,6 +3,12 @@ import { TopBar } from "../components/TopBar";
 import { ArrowbackIcon } from "../components/styles/Icons";
 import { useNavigate } from "react-router-dom";
 import { Screen } from "../components/styles/Screen";
+import { getApi } from "../api/getApi";
+import { useQuery } from "react-query";
+import { useEffect } from "react";
+import { getHeaders } from "../components/getHeaders";
+import { useLocalStorageToken } from "../hooks/useLocalStorageToken";
+import { SmallOrangeBtn } from "../components/styles/Button";
 
 export const BlockListScreen = styled(Screen)`
   padding-top: 80px;
@@ -11,13 +17,45 @@ export const BlockListScreen = styled(Screen)`
 
 export default function BlockList() {
   const navigate = useNavigate();
+  const token = useLocalStorageToken();
+  const headers = getHeaders(token);
+
+  const getBlockList = () =>
+    getApi({ link: `/users/me/blocked-users` }).then((response) =>
+      response.json()
+    );
+
+  const { data, isLoading, error } = useQuery(["myList"], getBlockList);
+
+  const onUnblockClick = (blockUserId: string | undefined) => {
+    console.log(blockUserId, "차단 해제");
+    getApi({ link: `/users/${blockUserId}/unblock` }).then(() => {});
+  };
+
+  useEffect(() => {
+    console.log(data.blockedUserOutputs);
+  }, []);
   return (
     <>
       <TopBar
         leftIcon={<ArrowbackIcon onClick={() => navigate(-1)} />}
         title="차단 유저 목록"
       />
-      <BlockListScreen>하잉</BlockListScreen>
+      <BlockListScreen>
+        {data?.blockedUserOutputs?.map((block: { nickname: string }) => (
+          <>
+            <div>{block?.nickname}</div>
+            <div>
+              <SmallOrangeBtn
+                text="차단 해제"
+                onClick={() => {
+                  onUnblockClick(block?.nickname);
+                }}
+              />
+            </div>
+          </>
+        ))}
+      </BlockListScreen>
     </>
   );
 }
