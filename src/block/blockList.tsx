caf0d6d594bceb27fff3e5d2e75e4d8bc6143438
kimgorok <kjyse2@naver.com>
2024-03-06 22:05:06 +0900

    🐢유저 차단 진행

--- src/block/blockList.tsx
+++ src/block/blockList.tsx
@@ -1,3 +1,23 @@
+import styled from "styled-components";
+import { TopBar } from "../components/TopBar";
+import { ArrowbackIcon } from "../components/styles/Icons";
+import { useNavigate } from "react-router-dom";
+import { Screen } from "../components/styles/Screen";
+
+export const BlockListScreen = styled(Screen)`
+  padding-top: 80px;
+  padding-bottom: 112px;
+`;
+
 export default function BlockList() {
-  return <h1>하이</h1>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <TopBar
+        leftIcon={<ArrowbackIcon onClick={() => navigate(-1)} />}
+        title="차단 유저 목록"
+      />
+      <BlockListScreen>하잉</BlockListScreen>
+    </>
+  );
 }
