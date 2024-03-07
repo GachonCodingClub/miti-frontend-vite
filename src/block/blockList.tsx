0e52d9e2893bb87cc1d5b5fd66212bb262982b43
kimgorok <kjyse2@naver.com>
2024-03-08 00:04:06 +0900

    ✅차단유저목록_차단해제

--- src/block/blockList.tsx
+++ src/block/blockList.tsx
@@ -1,26 +1,52 @@
 import { TopBar } from "../components/TopBar";
 import { ArrowbackIcon } from "../components/styles/Icons";
 import { useNavigate } from "react-router-dom";
-import { getApi } from "../api/getApi";
-import { Key, useEffect } from "react";
+import { Key } from "react";
 import { SmallOrangeBtn } from "../components/styles/Button";
 import { useGetBlockList } from "../api/blockList";
 import { BUserBox, BlockListScreen } from "./styles/BlockListComponents";
 import { UserName } from "../request-list/components/requestListComponents";
+import { getHeaders } from "../components/getHeaders";
+import { useLocalStorageToken } from "../hooks/useLocalStorageToken";
+import { GrayLine } from "../meeting-chat-room/styles/SideMenuComponents";
 
 export default function BlockList() {
   const navigate = useNavigate();
 
   const { data } = useGetBlockList();
+  const token = useLocalStorageToken();
+
+  const headers = getHeaders(token);
 
   const onUnblockClick = (blockUserId: string | undefined) => {
-    console.log(blockUserId, "차단 해제");
-    getApi({ link: `/users/${blockUserId}/unblock` }).then(() => {});
+    const PostUrl = `${
+      import.meta.env.VITE_BASE_URL
+    }/users/${blockUserId}/unblock`;
+    console.log("차단해제", blockUserId);
+    fetch(PostUrl, {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify({
+        blockUserId: blockUserId,
+      }),
+      headers: headers,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          console.error(
+            `API 오류 : ${response.status} - ${response.statusText}`
+          );
+          alert("서버 오류가 발생했습니다. 나중에 다시 시도해주세요.");
+          return response.json();
+        }
+        alert("해당 유저의 차단을 해제했어요.");
+        return response.json();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
-  useEffect(() => {
-    console.log(data.blockedUserOutputs);
-  }, []);
   return (
     <>
       <TopBar
@@ -29,18 +55,24 @@ export default function BlockList() {
       />
       <BlockListScreen>
         {data?.blockedUserOutputs?.map(
-          (block: { nickname: string }, index: Key | null | undefined) => (
-            <BUserBox key={index}>
-              <UserName>{block?.nickname}</UserName>
-              <div>
-                <SmallOrangeBtn
-                  text="차단 해제"
-                  onClick={() => {
-                    onUnblockClick(block?.nickname);
-                  }}
-                />
-              </div>
-            </BUserBox>
+          (
+            block: { nickname: string; userId: string },
+            index: Key | null | undefined
+          ) => (
+            <>
+              <BUserBox key={index}>
+                <UserName>{block?.nickname}</UserName>
+                <div>
+                  <SmallOrangeBtn
+                    text="차단 해제"
+                    onClick={() => {
+                      onUnblockClick(block?.userId);
+                    }}
+                  />
+                </div>
+              </BUserBox>
+              <GrayLine />
+            </>
           )
         )}
       </BlockListScreen>
