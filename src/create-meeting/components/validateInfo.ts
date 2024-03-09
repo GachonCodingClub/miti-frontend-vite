03fa5b0ebebff0f2802ccf019a150dc2747485bf
kimgorok <kjyse2@naver.com>
2024-03-09 12:03:04 +0900

    🔮방만들기_미팅 최대인원 설정

--- src/create-meeting/components/validateInfo.ts
+++ src/create-meeting/components/validateInfo.ts
@@ -8,7 +8,6 @@ export interface IValidationProps {
   setMemberError: React.Dispatch<string>;
   setDuplicateBlankErrorDialog: React.Dispatch<boolean>;
 }
-
 export const validateForm = ({
   selecteDate,
   inputPlace,
@@ -20,27 +19,43 @@ export const validateForm = ({
   setDuplicateBlankErrorDialog,
 }: IValidationProps): boolean => {
   const currentDate = new Date().toISOString().split("T")[0]; // 현재 날짜
+  let isValid = true;
+
   if (selecteDate === "") {
     setDateError("날짜를 선택해 주세요.");
+    isValid = false;
   } else if (selecteDate < currentDate) {
     setDateError("날짜는 오늘 이후여야 합니다.");
+    isValid = false;
   } else {
     setDateError(""); // 오류가 없을 경우 오류 메시지를 비웁니다.
   }
 
   setPlaceError(inputPlace === "" ? "장소를 입력해 주세요." : "");
-  setMemberError(numericInputMember < 2 ? "인원은 2 이상이어야 합니다." : "");
+
+  if (numericInputMember < 2) {
+    setMemberError("인원은 2명 이상이어야 해요.");
+    isValid = false;
+  } else if (numericInputMember > 50) {
+    setMemberError("최대 50명을 초과할 수 없어요.");
+    isValid = false;
+  } else {
+    setMemberError("");
+  }
 
   // 배열에 빈 문자열이 있는지 검사.
   if (additionalParticipants.includes("")) {
     setDuplicateBlankErrorDialog(true);
+    isValid = false;
   }
 
+  // 모든 검증을 통과했는지 여부 반환
   return (
-    selecteDate !== "" &&
+    isValid &&
     selecteDate >= currentDate &&
     inputPlace !== "" &&
     numericInputMember >= 2 &&
+    numericInputMember <= 50 &&
     !additionalParticipants.includes("")
   );
 };
