7c881a10a3d72c0784692a1cd6e7e073a3eeee6c
kimgorok <kjyse2@naver.com>
2024-05-27 19:51:32 +0900

    ✅유저 차단_차단 페이지 수정

--- src/block/index.tsx
+++ src/block/index.tsx
@@ -9,12 +9,17 @@ import { SettingButton } from "../profile/styles/settingComponents";
 import { ROUTES } from "../routes";
 import { LongOrangeBtn } from "../components/styles/Button";
 import { useState } from "react";
+import { useOneBtnDialog } from "../hooks/useOntBtnDialog";
+import { Dialog } from "../components/Dialog";
 
 export default function Block() {
   const navigate = useNavigate();
   const token = useLocalStorageToken();
   const headers = getHeaders(token);
   const [blockName, setBlockName] = useState("");
+
+  const { oneBtnDialog, showOneBtnDialog, hideOneBtnDialog } =
+    useOneBtnDialog();
 
   const onBlockChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newName = e.target.value;
@@ -38,15 +43,17 @@ export default function Block() {
           );
 
           if (response.status === 404) {
-            alert("닉네임을 확인해 주세요.");
+            showOneBtnDialog("닉네임을 확인해 주세요");
           } else if (response.status === 409) {
-            alert("이미 차단된 사용자예요.");
+            showOneBtnDialog("이미 차단한 사용자예요");
           } else {
-            alert("서버 오류가 발생했어요. 나중에 다시 시도해주세요.");
+            showOneBtnDialog(
+              "서버 오류가 발생했어요. 나중에 다시 시도해주세요"
+            );
           }
           return response.json();
         }
-        alert("해당 유저를 차단했어요.");
+        showOneBtnDialog("해당 유저를 차단했어요");
         return response.json();
       })
       .catch((error) => {
@@ -60,16 +67,13 @@ export default function Block() {
         title="차단하기"
       />
       <BlockListScreen>
-        <div className="py-8">
-          <p>차단할 유저의 닉네임을 입력해주세요.</p>
-          <p className="mt-3 mb-6">
-            채팅에서 유저를 선택하고 직접 차단할 수도 있어요.
-          </p>
+        <div>
+          <p>차단할 유저의 닉네임을 입력해주세요</p>
           <MyInputBox
-            label="차단하기"
+            label=""
             value={blockName}
             onChange={onBlockChange}
-            placeholder="차단할 유저의 닉네임을 입력하고 버튼을 눌러주세요."
+            placeholder="차단할 유저의 닉네임을 입력하고 버튼을 눌러주세요"
             type="text"
           />
           <div className="my-8">
@@ -86,6 +90,15 @@ export default function Block() {
             차단 유저 목록으로 이동
           </SettingButton>
         </div>
+
+        {oneBtnDialog.open && (
+          <Dialog
+            title={oneBtnDialog.title}
+            right="확인"
+            isOneBtn
+            onRightClick={hideOneBtnDialog}
+          />
+        )}
       </BlockListScreen>
     </>
   );
