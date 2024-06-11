a418262ea695f7ae311fe66edee8d0dd7f35414d
kimgorok <kjyse2@naver.com>
2024-06-12 02:23:18 +0900

    🔮링크 옆에 이동 버튼 추가

--- src/utils/linkify.tsx
+++ src/utils/linkify.tsx
@@ -5,9 +5,14 @@ export const linkify = (text: string): (string | JSX.Element)[] => {
     if (urlPattern.test(part)) {
       const url = part.startsWith("http") ? part : `https://${part}`;
       return (
-        <a key={index} href={url} target="_blank" rel="noopener noreferrer">
-          {part}
-        </a>
+        <div className="flex gap-2 items-center">
+          <a key={index} href={url} target="_blank" rel="noopener noreferrer">
+            {part}
+          </a>
+          <div className="bg-[#EBE8E7] px-[6px] py-[1px] text-sm rounded-3xl font-bold">
+            이동
+          </div>
+        </div>
       );
     }
     return part;
