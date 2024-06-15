f200f950689dbd3fa1c25085690a033e02dd6b2e
kimgorok <kjyse2@naver.com>
2024-06-15 15:49:36 +0900

    🔮사이드메뉴_한 줄 소개에 인스타 링크 있을 경우 아이콘 추가

--- src/utils/linkify.tsx
+++ src/utils/linkify.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { InstaIcon } from "../components/styles/Icons";
 
 interface LinkifyProps {
   text: string;
@@ -13,16 +14,27 @@ export const Linkify: React.FC<LinkifyProps> = ({ text, onLinkClick }) => {
       {text.split(urlPattern).map((part, index) => {
         if (urlPattern.test(part)) {
           const url = part.startsWith("http") ? part : `https://${part}`;
+          const isInstagram = url.includes("instagram.com");
+          // 인스타그램 아이디 추출 및 쿼리스트링 제거
+          let instagramId = null;
+          if (isInstagram) {
+            instagramId = url.split("instagram.com/")[1].split("?")[0];
+          }
+
           return (
             <a
               key={index}
               className="flex gap-2 items-center break-all break-words whitespace-pre-wrap"
               onClick={() => onLinkClick(url)}
             >
-              {part}
-              <p className="bg-[#EBE8E7] px-[6px] py-[1px] text-sm rounded-3xl font-bold whitespace-nowrap">
-                이동
-              </p>
+              {isInstagram ? instagramId : part}
+              {isInstagram ? (
+                <InstaIcon />
+              ) : (
+                <p className="bg-[#EBE8E7] px-[6px] py-[1px] text-sm rounded-3xl font-bold whitespace-nowrap">
+                  이동
+                </p>
+              )}
             </a>
           );
         }
